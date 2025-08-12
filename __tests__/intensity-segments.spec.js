6a38dd24a14a650f46d426c1512ebfeea1671dc3
Nathan <north.latitude.26@gmail.com>
2025-08-12 14:58:58 +0800

    test(__tests__): add test case for `#set` method.

--- __tests__/intensity-segments.spec.js
+++ __tests__/intensity-segments.spec.js
@@ -32,4 +32,17 @@ describe("major scenarios", () => {
     segments.add(10, 40, -1);
     expect(segments.toString()).toEqual('"[[10,-1],[20,0],[30,-1],[40,0]]"');
   });
+
+  test("should set segments and print serialized output properly", () => {
+    const segments = new IntensitySegments();
+
+    segments.add(10, 30, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[30,0]]"');
+
+    segments.add(20, 40, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[20,2],[30,1],[40,0]]"');
+
+    segments.set(10, 40, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[40,0]]"');
+  });
 });
