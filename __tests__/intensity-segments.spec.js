eee77a3db49d1146e7cb59692e449dc0bae06045
Nathan <north.latitude.26@gmail.com>
2025-08-12 15:05:27 +0800

    test(__tests__): add several special test cases to enhance robustness.

--- __tests__/intensity-segments.spec.js
+++ __tests__/intensity-segments.spec.js
@@ -2,6 +2,16 @@ import { test, describe, expect } from "@jest/globals";
 import { IntensitySegments } from "../src/intensity-segments";
 
 describe("major scenarios", () => {
+  test("#add and #set method should return its instance itself", () => {
+    const segments = new IntensitySegments();
+
+    const addHandle = segments.add(10, 30, 1);
+    expect(addHandle).toBeInstanceOf(IntensitySegments);
+
+    const setHandle = segments.set(10, 30, 1);
+    expect(setHandle).toBeInstanceOf(IntensitySegments);
+  });
+
   test("should add segments and print serialized output properly", () => {
     const segments = new IntensitySegments();
 
@@ -43,6 +53,21 @@ describe("major scenarios", () => {
     expect(segments.toString()).toEqual('"[[10,1],[20,2],[30,1],[40,0]]"');
 
     segments.set(10, 40, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[40,0]]"');
+  });
+});
+
+describe("sperical cases", () => {
+  test("should merge segments whose intensities are euqal and print serialized output properly", () => {
+    const segments = new IntensitySegments();
+
+    segments.add(10, 30, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[30,0]]"');
+
+    segments.add(20, 40, 1);
+    expect(segments.toString()).toEqual('"[[10,1],[20,2],[30,1],[40,0]]"');
+
+    segments.add(20, 30, -1);
     expect(segments.toString()).toEqual('"[[10,1],[40,0]]"');
   });
 });
